2f29a5ce0906599224492d1c22b46919ae2bdab5
JoacoRosas <jrosaszuiani@udesa.edu.ar>
2024-06-14 20:38:07 -0300

    avance con el saludo

--- js/saludo.js
+++ js/saludo.js
@@ -1,14 +1,14 @@
-let emailRecuperado =localStorage.getItem("email")
+let emailRecuperado = localStorage.getItem("email")
 let email = JSON.parse(emailRecuperado)
 console.log(email);
 
-let bienvenida = document.querySelector("nav")
+let bienvenida = document.querySelector("#saludo")
+
+let registro 
 
 if (email != null) {
     //muestro el saludo y oculto login y registro
-    alert("Bienvenido:" += email)
-
-    
+    bienvenida.innerHTML = `${"bienvenido : "+email}`
 }else{
     //muestro el registro de login y oculto el saludo
     
