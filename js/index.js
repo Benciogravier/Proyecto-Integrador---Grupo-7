5ef1e93437b466c821a0133f6a4546210ffc1367
Beniciogravier <150157673+Benciogravier@users.noreply.github.com>
2024-06-14 10:58:33 -0300

    Home seccion parte de beltran y benicio hecha juntos ya que realizamos un if dentro del fetch

--- js/index.js
+++ js/index.js
@@ -5,15 +5,15 @@ fetch('https://fakestoreapi.com/products/category/jewelery')
             .then(function(data){
                 console.log(data);
 
-                let productos = document.querySelector(".section-home")
+                let productos = document.querySelector(".section-home-joya")
                 let arrayProductos = data;
                 let productosHTML = ""
 
                 for(let i = 0; i < arrayProductos.length; i++){
                     productosHTML += 
-                    `<article class="art-home">
-                        <ul clas="lista-home">
-                            <li><img class=""img-home" src="${arrayProductos[i].image}"></li>
+                    `<article class="art-home-joya">
+                        <ul clas="lista-home-joya">
+                            <li><img class="img-home-joya" src="${arrayProductos[i].image}"></li>
                             <li><h2>${arrayProductos[i].title}</h2></li>
                             <li><p>${arrayProductos[i].description}</p></li>
                             <li><p>${arrayProductos[i].price}</p></li>
@@ -27,3 +27,46 @@ fetch('https://fakestoreapi.com/products/category/jewelery')
             .catch(function(error){
                 console.log(error);
             })
+fetch("https://fakestoreapi.com/products")
+        .then(function(res){
+            return res.json()
+        })
+        .then(function(data){
+            console.log(data);
+            let ropaHombre = document.querySelector(".section-home-men")
+            let RopaHRecorrida = ''
+            let ropaMujeres = document.querySelector(".section-home-women")
+            let mujeresRecorrido = ''
+            for (let i = 0; i < data.length; i++) {
+                if (data[i].category == "men's clothing") { //parte hecha por benicio
+                    RopaHRecorrida = RopaHRecorrida + `<article class="art-home-men">
+                        <ul class="lista-home-men">
+                            <img class="img-home-men" src="${data[i].image}">
+                            <h2>${data[i].title}</h2>
+                            <p>${data[i].description}</p>
+                            <p>${data[i].price} USD</p>
+                            <a href="./producto.html?id=${data[i].id}">
+                                <p>Ver mas</p>
+                            </a>
+                        </ul>
+                    </article>`
+                } else if (data[i].category == "women's clothing") { //parte hecha por beltran
+                    mujeresRecorrido = mujeresRecorrido + `<article class="art-home-women">
+                        <ul class="lista-home-women">
+                            <img class="img-home-women" src="${data[i].image}">
+                            <h2>${data[i].title}</h2>
+                            <p>${data[i].description}</p>
+                            <p>${data[i].price} USD</p>
+                            <a href="./producto.html?id=${data[i].id}">
+                                <p>Ver mas</p>
+                            </a>
+                        </ul>
+                    </article>`
+                }
+            }
+            ropaHombre.innerHTML = RopaHRecorrida
+            ropaMujeres.innerHTML = mujeresRecorrido
+        })
+        .catch(function(error){
+            console.log(error);
+        })
